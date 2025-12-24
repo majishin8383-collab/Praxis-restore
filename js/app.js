cb86b146007a8145823d69e828bd51d585a5d00d
majishin8383-collab <majishin8383@gmail.com>
2025-12-24 11:49:55 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,2 @@
 import { initRouter } from "./router.js";
-
-const main = document.getElementById("main");
-
-// PROBE: prove JS is running
-if (main) {
-  main.innerHTML = `
-    <div style="padding:16px;border:1px solid lime;border-radius:12px">
-      JS LOADED. ABOUT TO INIT ROUTER.
-    </div>
-  `;
-}
-
 initRouter();
